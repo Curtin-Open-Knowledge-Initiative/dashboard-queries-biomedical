a1cba32fa31582aef95edb65f1842179e87840b1
Bec Handcock <43615088+bechandcock@users.noreply.github.com>
2023-09-28 11:14:47 +0800

    dashboard_data_ver1l_2023_09_27_trialdata #COK-431

--- queries/dashboard_trialdata.sql
+++ queries/dashboard_trialdata.sql
@@ -1,253 +1,177 @@
 -----------------------------------------------------------------------
--- Montreal Neuro - Trial Data query
+-- Montreal Neuro - Data extract of the academic Observatory to 
+-- extract Crossref and Pubmed data and make a combined list of
+-- Clinical trials
 -----------------------------------------------------------------------
-DECLARE var_SQL_script_name STRING DEFAULT 'montreal_neuro_ver1l_2023_09_22_trialdata';
------------------------------------------------------------------------
--- 1. FUNCTIONS
------------------------------------------------------------------------
-
-# == FUNCTION ====================================
-CREATE TEMP FUNCTION function_cast_date(x ANY TYPE)
-AS (CAST(NULLIF(CAST(x AS STRING), "NA") AS DATE));
-
-# == FUNCTION ====================================
-CREATE TEMP FUNCTION function_cast_string(x ANY TYPE)
-AS (CAST(NULLIF(CAST(x AS STRING), "NA") AS STRING));
-
-# == FUNCTION ====================================
-CREATE TEMP FUNCTION function_cast_int(x ANY TYPE)
-AS (CAST(NULLIF(CAST(x AS STRING), "NA") AS INT));
-
-# == FUNCTION ====================================
-CREATE TEMP FUNCTION function_cast_boolean(x ANY TYPE)
-AS (CAST(NULLIF(CAST(x AS STRING), "NA") AS BOOLEAN));
-
-# == FUNCTION ====================================
-CREATE TEMP FUNCTION function_cast_datetime(x ANY TYPE)
-AS (
-   EXTRACT(DATE FROM (CAST(NULLIF(CAST(x AS STRING), "NA") AS TIMESTAMP)))
-   );
+DECLARE var_SQL_script_name STRING DEFAULT 'clintrial_extract_ver1l_2023_09_27';
 
 -----------------------------------------------------------------------
--- 2. PROCESS TRIAL DATA
+-- 0. EXTRACT AND TIDY FIELDS OF INTEREST
 -----------------------------------------------------------------------
-with trials_data AS (
+WITH main_select AS (
   SELECT
-  # ==== Metric name on dashboard: # Trials
-  function_cast_string(nct_id) as nct_id,
-  CASE
-    WHEN nct_id IS NULL THEN FALSE
-    ELSE TRUE
-    END as nct_id_found,
-  CASE
-    WHEN nct_id IS NULL THEN "No Trial-ID"
-    ELSE "Has Trial-ID"
-    END as nct_id_found_PRETTY,
+  ------ 0.1 DOI TABLE: Misc METADATA
+  academic_observatory.doi,
+  academic_observatory.crossref.published_year, -- from doi table
 
-  # ==== Metric name on dashboard: # Prospective registrations 
-  # has_summary_results IS A TEMPORARY CALC UNTIL THE VARIABLE is_prospective IS ADDED
-  function_cast_boolean(has_summary_results) as is_prospective,
+  ------ 0.2 CLINICAL TRIAL NUMBERS ASSOCIATED WITH PUBLICATIONS - CROSSREF - contained in fields
+  (SELECT array_agg(registry)[offset(0)]
+     FROM UNNEST(academic_observatory.crossref.clinical_trial_number))
+     AS CROSSREF_clintrial_fromfield_registry,
+
+  (SELECT array_agg(type)[offset(0)]
+     FROM UNNEST(academic_observatory.crossref.clinical_trial_number))
+     AS CROSSREF_clintrial_fromfield_type,
+
+  UPPER(TRIM((SELECT REPLACE(TRIM(array_agg(clinical_trial_number)[offset(0)]),'  ', ' ')
+     FROM UNNEST(academic_observatory.crossref.clinical_trial_number))))
+     AS CROSSREF_clintrial_fromfield_ids,
+
   CASE
-    WHEN function_cast_boolean(has_summary_results) IS TRUE THEN "Registered before enrollment started"
-    ELSE "Registered after enrollment started"
-    END as is_prospective_PRETTY,
+    WHEN academic_observatory.crossref.clinical_trial_number IS NULL THEN FALSE
+    WHEN ARRAY_LENGTH(academic_observatory.crossref.clinical_trial_number) > 0 THEN TRUE
+    ELSE FALSE
+  END as CROSSREF_clintrial_fromfield_found,
   
-  # ==== Metric name on dashboard: # Trial results in a registry < 1 year post completion 
-  # is_multicentric IS A TEMPORARY CALC UNTIL THE VARIABLE is_summary_results_1y IS ADDED
-  function_cast_boolean(is_multicentric) as is_summary_results_1y,
+  ------ 0.3 CLINICAL TRIAL NUMBERS ASSOCIATED WITH PUBLICATIONS - CROSSREF Abstract search for trial numbers
+  REGEXP_EXTRACT_ALL(UPPER(academic_observatory.crossref.abstract), r'NCT0\\d{7}') 
+    AS CROSSREF_clintrial_fromabstract_ids,
+
   CASE
-    WHEN function_cast_boolean(is_multicentric) IS TRUE THEN "Summary results reported within 1 year of trial completion"
-    ELSE "Summary results not reported within 1 year of trial completion"
-    END as is_summary_results_1y_PRETTY,
+    WHEN academic_observatory.crossref.abstract IS NULL THEN FALSE
+    WHEN academic_observatory.crossref.abstract = "" THEN FALSE
+    WHEN academic_observatory.crossref.abstract = "{}" THEN FALSE
+    WHEN REGEXP_CONTAINS(UPPER(academic_observatory.crossref.abstract), r'NCT0\\d{7}') THEN TRUE
+    ELSE FALSE
+  END as CROSSREF_clintrial_fromabstract_found,
 
-  function_cast_date(last_update_submitted_date) as last_update_submitted_date,
-  function_cast_date(registration_date) as registration_date,
-  function_cast_date(summary_results_date) as summary_results_date,
-  function_cast_string(study_type) as study_type,
-  function_cast_string(phase) as phase,
-  function_cast_int(enrollment) as enrollment,
-  function_cast_string(recruitment_status) as recruitment_status,
-  function_cast_string(title) as title,
-  function_cast_datetime(start_date) as start_date,
-  function_cast_datetime(completion_date) as completion_date,
-  function_cast_datetime(primary_completion_date) as primary_completion_date,
-  function_cast_boolean(has_summary_results) as has_summary_results,
-  function_cast_string(allocation) as allocation,
-  function_cast_string(masking) as masking,
-  function_cast_string(main_sponsor) as main_sponsor,
-  function_cast_boolean(is_multicentric) as is_multicentric,
-  function_cast_string(montreal_neuro_lead_sponsor) as montreal_neuro_lead_sponsor,
-  function_cast_string(montreal_neuro_principal_investigator) as montreal_neuro_principal_investigator,
-  function_cast_string(montreal_neuro_study_director) as montreal_neuro_study_director,
-  function_cast_string(montreal_neuro_study_chair) as montreal_neuro_study_chair,
-  function_cast_string(montreal_neuro_unspecified_official) as montreal_neuro_unspecified_official,
-  function_cast_string(montreal_neuro_responsible_party) as montreal_neuro_responsible_party,
-  function_cast_int(registration_year) as registration_year,
-  function_cast_int(start_year) as start_year,
-  function_cast_int(completion_year) as completion_year
-
-FROM `university-of-ottawa.neuro_data_raw.montreal_neuro-studies_ver1_raw`
-), # End of 2. SELECT trials_data
-
------------------------------------------------------------------------
--- 3. From the publication dataset get an exploded list of
---    all pairs of DOI and Trial-ID
------------------------------------------------------------------------
-d_pubsdata_ALL_pairs AS (
-  SELECT 
-  doi,
-  clintrial_CONCAT_ALL_SPLIT_unnested
-  FROM `university-of-ottawa.neuro_dashboard_data.dashboard_data`,
-  UNNEST (SPLIT(clintrial_CONCAT_ALL, ' ') ) AS clintrial_CONCAT_ALL_SPLIT_unnested
-  WHERE clintrial_CONCAT_ALL_SPLIT_unnested != ''
-  ), # END OF 3. SELECT d_pubsdata_ALL_pairs
-
------------------------------------------------------------------------
--- 4. GET DESIRED FIELDS OF THE PUBLICATION DATA
--- IS THIS REDUNDANT??
------------------------------------------------------------------------
- # data_pubs AS (
- # SELECT
- #   doi,
- #   published_year,
- #   container_title_concat,
- # REPLACE(TRIM(clintrial_CONCAT_ALL),'  ', ' ') AS clintrial_CONCAT_ALL
- # FROM
- #   `university-of-ottawa.neuro_dashboard_data.dashboard_data`
- #   ), # END OF 4. SELECT data_pubs
-
------------------------------------------------------------------------
--- 5. To the Trial Data, join matching dois from the publication set which reference 
---    those Trial-IDs and aggregate up
------------------------------------------------------------------------
-  pubsdata_MATCHED_pairs AS (
-   SELECT 
-   trials_data.nct_id,
-   # need to test with the real data if the following handles multiple dois found
-   string_agg(doi, ' ') as doi
-  
-   FROM trials_data
-   LEFT JOIN d_pubsdata_ALL_pairs
-   ON LOWER(d_pubsdata_ALL_pairs.clintrial_CONCAT_ALL_SPLIT_unnested) LIKE 
-     CONCAT('%', LOWER(TRIM(trials_data.nct_id)), '%' )
-   group by trials_data.nct_id
-  ), # END OF 5. SELECT pubsdata_MATCHED_pairs
-
------------------------------------------------------------------------
--- 6. To the Trial Data, join matching publications which reference those Trial-IDs
------------------------------------------------------------------------
-trial_data_and_pubs AS (
-  SELECT
-  trials_data.*,
-  pubsdata_MATCHED_pairs.* EXCEPT (nct_id),
-  CASE
-    WHEN doi IS NULL THEN FALSE
-    ELSE TRUE
-    END as doi_found,
-  CASE
-    WHEN doi IS NULL THEN "No reference of Trial dataset Trial-IDs in publication"
-    ELSE "Trial dataset Trial-IDs referenced in publication"
-  END as doi_found_PRETTY,
-
-  ----- UTILITY - add a variable for the script version
-  var_SQL_script_name
-
-  FROM trials_data
-  LEFT JOIN pubsdata_MATCHED_pairs 
-  ON trials_data.nct_id = pubsdata_MATCHED_pairs.nct_id
-  ) # END OF 6. SELECT trial_data_and_pubs
-
------------------------------------------------------------------------
--- 7. Now do the Some for Crossref/Pubmed
---    From the Crossref/Pubmed dataset get a list of
---    all pairs of DOI and Trial-ID
------------------------------------------------------------------------
-/*table_crossref_pubmed AS (
-  ------ 7.1 TABLES.
-  SELECT academic_observatory.doi,
-  -----------------------------------------------------------------------
-  --  Search CROSSREF for Trial-IDs
-  -----------------------------------------------------------------------
-  ------ 7.2 CLINICAL TRIAL NUMBERS ASSOCIATED WITH PUBLICATIONS - CROSSREF - contained in fields
-  TRIM((SELECT REPLACE(TRIM(array_agg(clinical_trial_number)[offset(0)]),'  ', ' ')
-     FROM UNNEST(academic_observatory.crossref.clinical_trial_number)))
-     AS clintrial_crossref_id,
-
-  ------ 7.3 CLINICAL TRIAL NUMBERS ASSOCIATED WITH PUBLICATIONS - CROSSREF Abstract search for trial numbers
-  REGEXP_EXTRACT_ALL(UPPER(academic_observatory.crossref.abstract), r'NCT0\\d{7}')
-    AS clintrial_crossref_id_fromabstract,
-
-  -----------------------------------------------------------------------
-  -- Search Pubmed for Trial-IDs
-  -----------------------------------------------------------------------
-  ------ 7.4 PUBMED TABLE: CONCATENATED Clinical Trial Registries/Data Banks, and Accession Numbers (optional fields)
-  TRIM((SELECT STRING_AGG(id, " ") FROM UNNEST(pubmed.pubmed_DataBankList))) AS clintrial_pubmed_id_CONCAT,
-
-  "" AS clintrial_pubmed_id_fromabstract,
-  
-  FROM `academic-observatory.observatory.doi20230618` as academic_observatory
-  # PubMed is only included here as the required fields are not yet in the Academic Observatory
-  LEFT JOIN `university-of-ottawa.neuro_data_processed.pubmed_extract` as pubmed
-  ON LOWER(academic_observatory.doi) = LOWER(pubmed_doi)
-  
-  ), # END OF 7. SELECT table_crossref_pubmed
-
------------------------------------------------------------------------
--- 8. Combine Crossref and Pubmed
------------------------------------------------------------------------
-combine_table_crossref_pubmed AS (
-  SELECT
-  doi,
-  #------ combine Trial-IDs from datasets
-  REPLACE(
-    CONCAT(
-      COALESCE(clintrial_crossref_id,""), " ",
-      # The following complex statement is to capture missing (?) values that returns "0 rows" isntead of null
-      TRIM(COALESCE(
-        (SELECT CONCAT(p1)
-        FROM UNNEST(clintrial_crossref_id_fromabstract) AS p1)
-        ,'')),
-      COALESCE(clintrial_pubmed_id_CONCAT,""), " ", 
-      COALESCE(clintrial_pubmed_id_fromabstract,""), " "
-       ), '  ',' ') AS clintrial_CONCAT_ALL
-
-FROM table_crossref_pubmed
-
-), # END OF 8. SELECT combine_table_crossref_pubmed AS (
-
-
------------------------------------------------------------------------
--- 9. To the Trial Data, join matching publication dois which reference 
---    those Trial-IDs and aggregate up
------------------------------------------------------------------------
------------------------------------------------------------------------
--- 6. To the Trial Data, join matching publications which reference those Trial-IDs
------------------------------------------------------------------------
-trial_data_and_pubs AS (
-  SELECT
-  trials_data.*,
-  pubsdata_MATCHED_pairs.* EXCEPT (nct_id),
-  CASE
-    WHEN doi IS NULL THEN FALSE
-    ELSE TRUE
-    END as doi_found,
-  CASE
-    WHEN doi IS NULL THEN "No reference of Trial dataset Trial-IDs in publication"
-    ELSE "Trial dataset Trial-IDs referenced in publication"
-  END as doi_found_PRETTY,
-
-  ----- UTILITY - add a variable for the script version
-  var_SQL_script_name
-
-  FROM trials_data
-  LEFT JOIN d_pubsdata_MATCHED_pairs 
-  ON trials_data.nct_id = d_pubsdata_MATCHED_pairs.nct_id
-  ) # END OF 6. SELECT trial_data_and_pubs
-
-*/
-SELECT
-*
-FROM
-trial_data_and_pubs
-
+  ------ 0.4 ABSTRACTS from any sources
+  academic_observatory.crossref.abstract AS abstract_CROSSREF,
+  pubmed.pubmed_Abstract AS abstract_PUBMED,
  
+  ------ 0.5 PUBMED TABLE: CONCATENATED Clinical Trial Registries/Data Banks,and Accession Numbers (optional fields)
+  ------- This field is overloaded, requirin special processing!
+  pubmed.pubmed_DataBankList AS pubmed_DataBankList_RAW,
+  TRIM((SELECT STRING_AGG(name, " ") FROM UNNEST(pubmed.pubmed_DataBankList))) AS PUBMED_DataBankList_names_CONCAT,
+  TRIM((SELECT STRING_AGG(id, " ") FROM UNNEST(pubmed.pubmed_DataBankList))) AS PUBMED_DataBankList_ids_CONCAT,
+
+  "" AS PUBMED_clintrial_fromabstract_ids,
+  FALSE AS PUBMED_clintrial_fromabstract_found,
+ -----------------------------------------------------------------------
+ FROM
+    ------ Crossref from Academic Observatory.
+    `academic-observatory.observatory.doi20230618` as academic_observatory
+    # PubMed is only included here as the required fields are not yet in the Academic Observatory
+    LEFT JOIN `university-of-ottawa.neuro_data_processed.pubmed_extract` as pubmed
+      ON LOWER(academic_observatory.doi) = LOWER(pubmed_doi)
+      WHERE academic_observatory.crossref.published_year > 2000
+
+ ), # END OF 0. SELECT main_select
+
+-----------------------------------------------------------------------
+-- 1 Flatten the nested Pubmed data
+-----------------------------------------------------------------------
+table_1 AS (
+SELECT 
+  doi,
+  pubmed_DataBankList_unested1.name AS PUBMED_DataBankList_names,
+  pubmed_DataBankList_unested1.id AS PUBMED_DataBankList_ids,
+FROM
+  main_select ,
+  UNNEST(pubmed_DataBankList_RAW) AS pubmed_DataBankList_unested1
+), # END. SELECT table_1
+
+-----------------------------------------------------------------------
+-- 2: Extract just the CLINICAL TRIAL Registries from the overloaded field
+-----------------------------------------------------------------------
+table_2 AS (
+SELECT
+  doi,
+  STRING_AGG(PUBMED_DataBankList_names, ' ') AS PUBMED_clintrial_fromfield_names,
+  UPPER(STRING_AGG(PUBMED_DataBankList_ids, ' ')) AS PUBMED_clintrial_fromfield_ids
+  FROM table_1
+    WHERE REGEXP_CONTAINS(PUBMED_DataBankList_names,'ANZCTR|ChiCTR|CRiS|ClinicalTrials\\.gov|CTRI|DRKS|EudraCT|IRCT|ISRCTN|JapicCTI|JMACCT|JPRN|NTR|PACTR|ReBec|REPEC|RPCEC|SLCTR|TCTR|UMIN CTR|UMIN-CTR')
+    GROUP by doi
+), # END. SELECT table_2
+
+-----------------------------------------------------------------------
+-- 3: Link CLINICAL TRIAL Registries to main query
+-----------------------------------------------------------------------
+table_3 AS (
+SELECT
+  main_select.*,
+  table_2_joined.PUBMED_clintrial_fromfield_names,
+  table_2_joined.PUBMED_clintrial_fromfield_ids,
+  CASE
+    WHEN table_2_joined.PUBMED_clintrial_fromfield_ids IS NULL THEN FALSE
+    WHEN LENGTH(table_2_joined.PUBMED_clintrial_fromfield_ids) > 0 THEN TRUE
+    ELSE FALSE
+  END AS PUBMED_clintrial_fromfield_found
+
+FROM main_select
+  FULL JOIN table_2 as table_2_joined
+  ON LOWER(main_select.doi) = LOWER(table_2_joined.doi)
+), # END. SELECT table_3
+
+-----------------------------------------------------------------------
+-- 4: Combine trial IDs and registies for BOTH Crossref and Pubmed
+-----------------------------------------------------------------------
+table_4 AS (
+  SELECT
+  * EXCEPT (pubmed_DataBankList_RAW),  
+  ------ 4.1 Clinical Trial - combine Trial-IDs from all sources
+    TRIM(REPLACE(
+        CONCAT(COALESCE(CROSSREF_clintrial_fromfield_ids,""), " ",
+        # The following complex statement is to capture missing (?) values that returns "0 rows" isntead of null
+        TRIM(COALESCE((SELECT CONCAT(p1) FROM UNNEST(CROSSREF_clintrial_fromabstract_ids) AS p1) ,'')),
+        COALESCE(PUBMED_clintrial_fromfield_ids,""), " ", 
+        COALESCE(PUBMED_clintrial_fromabstract_ids,""), " "
+       ),'  ',' ')) AS ANYSOURCE_clintrials,
+
+  ------ 4.2 Determine if ANY Clinical Trial is found from ANY source
+   IF (CROSSREF_clintrial_fromfield_found 
+    OR CROSSREF_clintrial_fromabstract_found
+    OR PUBMED_clintrial_fromfield_found
+    OR PUBMED_clintrial_fromabstract_found, 
+    TRUE, FALSE) AS ANYSOURCE_clintrial_found
+  
+  FROM table_3
+  ), # END SELECT table 4
+
+-----------------------------------------------------------------------
+-- 5: Extract just the DATABANKS from the overloaded field
+-----------------------------------------------------------------------
+table_5 AS (
+SELECT
+  doi,
+  STRING_AGG(PUBMED_DataBankList_names, ' ') AS PUBMED_opendata_fromfield_names,
+  STRING_AGG(PUBMED_DataBankList_ids, ' ') AS PUBMED_opendata_fromfield_ids
+FROM table_1
+WHERE REGEXP_CONTAINS(PUBMED_DataBankList_names,'BioProject|dbGaP|dbSNP|dbVar|Dryad|figshare|GDB|GENBANK|GEO|OMIM|PIR|PubChem-BioAssay|PubChem-Compound|PubChem-Substance|RefSeq|SRA|SWISSPROT|UniMES|UniParc|UniProtKB|UniRef|PDB|Protein')
+
+GROUP by doi
+) # END. SELECT table_5
+
+-----------------------------------------------------------------------
+-- 6: Link Pubmed Databanks to main query
+-----------------------------------------------------------------------
+SELECT
+  ----- 6.1 Link Pubmed Databanks to main query
+  table_4.*,
+  table5_joined.PUBMED_opendata_fromfield_names,
+  table5_joined.PUBMED_opendata_fromfield_ids,
+  CASE
+    WHEN table5_joined.PUBMED_opendata_fromfield_ids IS NULL THEN FALSE
+    WHEN LENGTH(table5_joined.PUBMED_opendata_fromfield_ids) > 0 OR LENGTH(table5_joined.PUBMED_opendata_fromfield_names) > 0 THEN TRUE
+    ELSE FALSE
+  END AS PUBMED_opendata_fromfield_found,
+
+  ----- 6.2 UTILITY - add a variable for the script version
+  var_SQL_script_name
+
+  FROM table_4
+  FULL JOIN table_5 as table5_joined
+  ON LOWER(table_4.doi) = LOWER(table5_joined.doi)
+  
+  # END.main SELECT
+-----------------------------------------------------------------------
