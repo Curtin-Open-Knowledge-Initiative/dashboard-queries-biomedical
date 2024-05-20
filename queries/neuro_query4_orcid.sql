14e9f2d0c9ca387a08f98f719894a81ddbe5d866
Bec Handcock <43615088+bechandcock@users.noreply.github.com>
2024-05-20 14:07:46 +0800

    neuro_ver1p_query4_orcid_2024_05_17 #BAD-478 #BAD479
    Tidied code and naming conventions as part of edits following Phase 1 user testing

--- queries/neuro_query4_orcid.sql
+++ queries/neuro_query4_orcid.sql
@@ -2,10 +2,10 @@
 -- Montreal Neuro - Trial Data query 
 -- Run this 4th and cascade to "dashboard_data_orcids"
 -----------------------------------------------------------------------
-###---###---###---###---###---### CHECK INPUTS BELOW FOR CORRECT VERSION
-DECLARE var_SQL_script_name STRING DEFAULT 'neuro_ver1o_query4_orcid_2024_01_19';
+###---###---###---###---###---### CHECK INPUTS BELOW FOR CORRECT VERSIONS
+DECLARE var_SQL_script_name STRING DEFAULT 'neuro_ver1p_query4_orcid_2024_05_17';
 DECLARE var_ORCID_Dataset_name STRING DEFAULT 'theneuro_orcids_20230906';
-DECLARE var_output_table STRING DEFAULT 'dashboard_data_ver1o_2024_01_19_orcid';
+DECLARE var_output_table STRING DEFAULT 'OUTPUT_ver1p_query4_orcid_2024_05_17';
 
 -----------------------------------------------------------------------
 -- 1. FUNCTIONS
@@ -21,8 +21,8 @@ AS (CAST(NULLIF(CAST(x AS STRING), "NA") AS BOOLEAN));
 -----------------------------------------------------------------------
 -- 0. Setup table 
 -----------------------------------------------------------------------
-###---###---###---###---###---### CHECK INPUTS BELOW FOR CORRECT VERSION
-CREATE TABLE `university-of-ottawa.neuro_dashboard_data_archive.dashboard_data_ver1o_2024_01_19_orcid`
+###---###---###---###---###---### CHECK OUTPUT BELOW FOR CORRECT VERSION
+CREATE TABLE `university-of-ottawa.neuro_dashboard_data_archive.OUTPUT_ver1p_query4_orcid_2024_05_17`
  AS (
 
 -----------------------------------------------------------------------
@@ -37,28 +37,34 @@ with orcid_data AS (
 
    # ---------- orcid_id
    function_cast_string(orcid_id) as orcid_id_raw,
-   CASE WHEN is_orcid THEN orcid_id
+
+   CASE WHEN is_orcid THEN function_cast_string(orcid_id)
      ELSE NULL
      END as orcid_id,
 
    # ---------- is_orcid
    function_cast_boolean(is_orcid) as is_orcid,
+
    CASE WHEN is_orcid THEN "ORCID found for researcher"
      ELSE "No ORCID found for researcher"
      END as is_orcid_PRETTY,
 
    # ---------- orcid_verified
    function_cast_boolean(orcid_verified) as orcid_verified,
+
    CASE WHEN orcid_verified THEN "Verified ORCID found for researcher"
      ELSE "No verified ORCID found for researcher"
-     END as orcid_verified_PRETTY,   
+     END as orcid_verified_PRETTY,  
+      
    # ---------- is_affiliation
    function_cast_boolean(is_affiliation) as is_affiliation,
+
    CASE WHEN is_affiliation THEN "ORCID affiliation found for researcher"
      ELSE "No ORCID affiliation found for researcher"
      END as is_affiliation_PRETTY,
 
 ###---###---###---###---###---### CHECK INPUTS BELOW FOR CORRECT VERSION
+# of the contributed ORCID data from the partner.
 FROM `university-of-ottawa.neuro_data_processed.theneuro_orcids_20230906`
 
 ) # End of 2. SELECT orcid_data
