2fa603441a8a429e842615631cafccc177824ec2
keeagnsmith21 <keegan.r.s21@gmail.com>
2025-03-06 16:47:57 +0800

    Config flexibility update

--- biomed/main.py
+++ biomed/main.py
@@ -2,11 +2,11 @@ import argparse
 import yaml
 
 from biomed.gcp import gcp_set_auth, bq_run_query
-from biomed.config import Config
+from biomed.config import Config, Partner, Context
 
 
 def workflow(config: Config):
-    gcp_set_auth(config.keyfile)
+    gcp_set_auth(config.context.keyfile)
     query = """
     SELECT name, SUM(number) as total_people
     FROM `bigquery-public-data.usa_names.usa_1910_2013`
@@ -15,9 +15,15 @@ def workflow(config: Config):
     ORDER BY total_people DESC
     LIMIT 20
 """
-    rows = bq_run_query(query=query)
-    for row in rows:
-        print(row)
+    for partner in config.partners:
+        partner_workflow(partner, config.context)
+    #     rows = bq_run_query(query=query)
+    # for row in rows:
+    #     print(row)
+
+
+def partner_workflow(partner: Partner, context: Context):
+    print(partner.name)
 
 
 def main():
@@ -30,7 +36,7 @@ def main():
         help="The config file containing the run information. See example_config.yaml for an example of the config expectations.",
     )
     args = parser.parse_args()
-    with open(args.cfg_file, "r") as f:
+    with open(args.config, "r") as f:
         yaml_cfg = yaml.safe_load(f)
     config: Config = Config.from_dict(yaml_cfg)
     workflow(config)
