ebe2af1f4592120f534d0a33b65941b18f231188
Bec Handcock <43615088+bechandcock@users.noreply.github.com>
2025-03-10 00:44:52 +0800

    p01_ver2b_query2_trials_20250310 #COK-509

--- queries/dashboard_query2_trials.sql
+++ queries/dashboard_query2_trials.sql
@@ -1,16 +1,15 @@
 -----------------------------------------------------------------------
--- Montreal Neuro - Trial Data query 
--- Run this second
--- See instructions at https://github.com/Curtin-Open-Knowledge-Initiative/dashboard-queries-biomedical
--- The in put trials data was created here: https://github.com/maia-sh/the-neuro-trials/tree/main
+-- Biomedical Open Science Dashboard Processing - Process Clinical Trial data
+-- RUN THIS SECOND
+-- See https://github.com/Curtin-Open-Knowledge-Initiative/dashboard-queries-biomedical
 -----------------------------------------------------------------------
 
 ###---###---###---###---###---### CHECK INPUTS BELOW FOR CORRECT VERSIONS
-DECLARE var_SQL_script_name STRING DEFAULT 'p01_ver2b_query2_trials_20250305';
-DECLARE var_output_table STRING DEFAULT 'p01_trials_20250305';
+DECLARE var_SQL_script_name STRING DEFAULT 'p01_ver2b_query2_trials_20250310';
+DECLARE var_output_table STRING DEFAULT 'p01_trials_20250310';
 
-DECLARE var_data_trials STRING DEFAULT 'theneuro_trials_20231111';
-DECLARE var_data_dois STRING DEFAULT 'theneuro_dois_20230217';
+DECLARE var_data_trials STRING DEFAULT 'p01_theneuro_trials_aact_20250221';
+DECLARE var_data_dois STRING DEFAULT 'p01_theneuro_dois_20230217';
 DECLARE var_institution_id STRING DEFAULT 'p01_theneuro';
 
 -----------------------------------------------------------------------
@@ -43,16 +42,14 @@ AS (
 -- 2. Setup table 
 -----------------------------------------------------------------------
 ####---###---###---###---###---### CHECK OUTPUT BELOW FOR CORRECT VERSION
-CREATE TABLE `university-of-ottawa.p01_neuro_data.p01_trials_20250305`
+CREATE TABLE `university-of-ottawa.p01_neuro_data.p01_trials_20250310`
  AS (
 
 -----------------------------------------------------------------------
--- 3. PROCESS IMPORTED TRIAL DATA
+-- 3. PROCESS IMPORTED TRIAL DATA - this is the export from the AACT processing
 -----------------------------------------------------------------------
 with d_3_contributed_trials_data AS (
   SELECT
-  # descriptions of fields are here: https://github.com/maia-sh/the-neuro-trials/blob/main/R/03_combine-trials.R
-  
   # ==== Metric name on dashboard: # Trials
   UPPER(function_cast_string(nct_id)) as nct_id,
   CASE
@@ -79,16 +76,6 @@ with d_3_contributed_trials_data AS (
     ELSE "Registered after enrollment started"
     END as is_prospective_PRETTY,
  
-  #function_cast_date(registry_download_date) as registry_download_date, # New field for Phase 2
-  #function_cast_date(results_due_date) as results_due_date, # New field for Phase 2
-  
-  # ==== Metric name on dashboard: # Results Due 
-  function_cast_boolean(results_due) as results_due, # New field for Phase 2
-  CASE
-    WHEN function_cast_boolean(results_due) IS TRUE THEN "Trial results are due"
-    ELSE "Trial results are not yet due"
-    END as results_due_PRETTY,
-
   function_cast_int(days_pcd_to_summary) as days_pcd_to_summary,
 
   # ==== Metric name on dashboard: # Trial results in a registry < 1 year post completion 
@@ -113,7 +100,7 @@ with d_3_contributed_trials_data AS (
     ELSE 99
     END as summary_results_reporting_GRAPHORDER,
 
-   # ==== Metric name on dashboard: # Linked References in clintrials.gov
+   # ==== Metric name on dashboard: # Trials with linked references
   function_cast_boolean(has_linked_reference) as has_linked_reference,# New field for Phase 2
   CASE
     WHEN function_cast_boolean(has_linked_reference) IS TRUE THEN "Trial has a linked reference"
@@ -121,8 +108,8 @@ with d_3_contributed_trials_data AS (
     END as has_linked_reference_PRETTY,
 
 ##---###---###---###---###---### CHECK INPUTS BELOW FOR CORRECT VERSION
-# of the imported trials from the partner.
-FROM `university-of-ottawa.p01_neuro_from_partners.theneuro_trials_aact_20250221`
+# of the imported trials from the partner institution and processed by the BOS project team
+FROM `university-of-ottawa.p01_neuro_from_partners.p01_theneuro_trials_aact_20250221`
 ), # End of d_3_contributed_trials_data
 
 -----------------------------------------------------------------------
@@ -141,7 +128,7 @@ SELECT
 FROM
   ###---###---###---###---###---### CHECK OUTPUT BELOW FOR CORRECT VERSION 
   # of the all trials processed in Step 1 - query1
-  `university-of-ottawa.p01_neuro_data.p01_alltrials_20250305`,
+  `university-of-ottawa.p01_neuro_data.p01_alltrials_20250310`,
   UNNEST(SPLIT(TRIM(ANYSOURCE_clintrial_idlist)," ")) as ANYSOURCE_clintrial_id_flat
   WHERE ANYSOURCE_clintrial_found
 ), # END d_4_anysource_extract_flat
@@ -149,14 +136,14 @@ FROM
 -----------------------------------------------------------------------
 -- STEP 5:
 -- These next steps are to get the data that inthe dashboard this will go 
--- into the section titled "Linking The Neuro's trial dataset to The Neuro's publications"
+-- into the section on linking the institutions trial dataset to the institutions publications
 -- Data from Crossref and PubMed is used to link the two datasets
 -----------------------------------------------------------------------
 
 -----------------------------------------------------------------------
 -- 5A. To the list of Contributed trial IDs (ie #3), join to the flattened table of 
 -- Trial-ID/DOIs from Crossref/Pubmed (ie #4), using the trial IDs
--- to match the tables. Also join the 
+-- to match the tables. 
 -----------------------------------------------------------------------
 d_5a_trials_joined_to_anysource AS (
   SELECT 
@@ -179,31 +166,18 @@ d_5a_trials_joined_to_anysource AS (
 d_5b_trials_data_joined_to_anysource AS (
   SELECT 
   p4.*,
-  p5.ANYSOURCE_dois_matching_trialid,
+  p5.ANYSOURCE_dois_matching_trialid
 
-  CASE
-    WHEN p5.ANYSOURCE_dois_matching_trialid IS NOT NULL
-    THEN TRUE
-    ELSE FALSE
-    END AS ANYSOURCE_doi_found,
-
-  CASE
-    WHEN p5.ANYSOURCE_dois_matching_trialid IS NOT NULL
-    THEN "Trial-IDs from the Neuro's trial dataset found in a Crossref or Pubmed publication"
-    ELSE "No Trial-IDs from the Neuro's trial dataset found in a Crossref or Pubmed publication"
-    END AS ANYSOURCE_doi_found_PRETTY,
-
-  FROM d_3_contributed_trials_data as p4
-  LEFT JOIN d_5a_trials_joined_to_anysource as p5
-    ON lower(p4.nct_id) = lower(p5.nct_id)  
-  # END OF d_5b_trials_data_joined_to_anysource
+FROM d_3_contributed_trials_data as p4
+LEFT JOIN d_5a_trials_joined_to_anysource as p5
+  ON lower(p4.nct_id) = lower(p5.nct_id)  
+# END OF d_5b_trials_data_joined_to_anysource
 ),
 
 -----------------------------------------------------------------------
 -- STEP 6:
 -- These next steps are to get the data that inthe dashboard this will go 
--- into the section titled "Trial-IDs from The Neuro's trial dataset that 
--- are found in The Neuro's manuscript-style publication dataset"
+-- into the section on linking trials and publications.
 -- Data from Crossref and PubMed is used to link the two datasets
 -----------------------------------------------------------------------
 
@@ -220,8 +194,8 @@ d_6a_pubs_data_intersect_anysource AS (
   p7.ANYSOURCE_clintrial_id_flat
   FROM
     ###---###---###---###---###---### CHECK INPUTS BELOW FOR CORRECT VERSION
-    # of the imported dois from the partner.
-    `university-of-ottawa.p01_neuro_from_partners.theneuro_dois_20230217` as p6
+    # of the imported dois from the partner institution
+    `university-of-ottawa.p01_neuro_from_partners.p01_theneuro_dois_20230217` as p6
     INNER JOIN d_4_anysource_extract_flat as p7
       ON LOWER(p6.doi) = LOWER(p7.ANYSOURCE_doi)
 ), # END OF d_6a_pubs_data_intersect_anysource
@@ -259,20 +233,20 @@ SELECT
     WHEN p11.PUBSDATA_doi IS NOT NULL
     THEN TRUE
     ELSE FALSE
-    END AS PUBSDATA_doi_found,
+    END AS PUBSDATA_doi_found, ##### KEEP
 
   CASE
     WHEN p11.PUBSDATA_doi IS NOT NULL
-    THEN "Trial-IDs from the Neuro's trial dataset found in a publication from The Neuro"
-    ELSE "No Trial-IDs from the Neuro's trial dataset found in a publication from The Neuro"
-    END AS PUBSDATA_doi_found_PRETTY,
+    THEN "Trial-IDs from the institution's trial dataset found in a publication from the institution"
+    ELSE "No Trial-IDs from the institution's trial dataset found in a publication from the institution"
+    END AS PUBSDATA_doi_found_PRETTY, ##### KEEP
 
   ----- UTILITY - add a variable for the script and input data versions
   var_SQL_script_name,
   var_data_trials,
   var_data_dois,
   var_output_table,
-  var_institution_id
+  var_institution_id,
 
   FROM d_5b_trials_data_joined_to_anysource as p10
   LEFT JOIN d_6b_pubs_data_intersect_anysource as p11 
